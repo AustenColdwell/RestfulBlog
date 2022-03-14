3e82388d696cc369cfc2d3d7a2d82898239188ab
Austen Coldwell <austen.coldwell@gmail.com>
2022-03-14 13:51:34 +0000

    Added createComment functionality to the service and controller

---
+++ src/main/java/com/springboot/restfulblog/controller/CommentController.java
@@ -0,0 +1,27 @@
+package com.springboot.restfulblog.controller;
+
+import com.springboot.restfulblog.payload.CommentDto;
+import com.springboot.restfulblog.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    //========== CREATE ==========
+    @PostMapping("/post/{postId}/comment")
+    public ResponseEntity<CommentDto> createComment(
+            @PathVariable(value = "postId") long postId,
+            @RequestBody CommentDto commentDto)
+    {
+        return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
+    }
+}
