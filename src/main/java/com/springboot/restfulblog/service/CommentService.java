fccc76a2cde4fc18e86d918551d0ed18f66b271c
Austen Coldwell <austen.coldwell@gmail.com>
2022-03-15 12:51:06 +0000

    Added updateCommentById to service and controller

--- src/main/java/com/springboot/restfulblog/service/CommentService.java
+++ src/main/java/com/springboot/restfulblog/service/CommentService.java
@@ -12,4 +12,6 @@ public interface CommentService {
 
     CommentDto getCommentById(long postId, long commentId);
 
+    CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest);
+
 }
